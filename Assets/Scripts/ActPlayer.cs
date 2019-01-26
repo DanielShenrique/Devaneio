cff215e1dce7836895af56e5a4f699d9b9fe5ec8
Aluno <aluno@cejoseleitelopes.com.br>
2019-01-26 17:20:18 -0200

    1
    1

---
+++ Assets/Scripts/ActPlayer.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ActPlayer : MonoBehaviour {
+
+	public float speed;
+	
+	public Rigidbody2D rb;
+	
+    public GameObject b;
+
+    public Text text;
+
+    public GameObject oTexto;
+	
+	Camera cameraz;
+	
+	// Use this for initialization
+	void Start () {
+		 cameraz = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		float moveH = Input.GetAxisRaw("Horizontal");
+		float moveV = Input.GetAxisRaw("Vertical");
+		
+		rb.velocity = new Vector2(moveH * speed, moveV * speed);
+	}
+
+	
+	void OnTriggerEnter2D(Collider2D cool)
+	{
+			if(cool.gameObject.CompareTag("Final"))
+			{
+				StartCoroutine(Zoom());			
+			}
+	}
+    void OnTriggerStay2D(Collider2D cool)
+    {
+        if (cool.gameObject.CompareTag("Aviso"))
+        {
+            text.GetComponent<Alerta>().Foi = true;
+            oTexto.SetActive(true);
+        }
+    }
+	void OnTriggerExit2D(Collider2D cool)
+	{
+		if(cool.gameObject.CompareTag("Aviso"))
+		{
+			text.GetComponent<Alerta>().Foi = false;
+            oTexto.SetActive(false);
+		}
+	}
+	
+	IEnumerator Zoom()
+	{
+		for (float z = 5f; z<10f; z+=0.05f) {
+			cameraz.orthographicSize = z;
+			yield return null;
+		}
+		for (float z = 10f; z >0f; z-=0.05f) {
+			cameraz.orthographicSize = z;
+			yield return null;	
+		}
+		SceneManager.LoadScene(1);
+	}
+}
---
+++ Assets/Scripts/Alerta.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Alerta : MonoBehaviour {
+
+    private Text _textComponent;
+
+    public string[] DialogueStrings;
+
+    public float SecondsBetweenCharacters = 1f;
+    public float CharacterRateMultiplier = 0.5f;
+
+    public KeyCode DialogueInput = KeyCode.Z;
+
+    public bool Foi;
+
+    public GameObject Texto;
+
+    private bool _isDialoguePlaying = false;
+
+    //public GameObject proximoPainel;
+    // Use this for initialization
+    void Start()
+    {
+        _textComponent = GetComponent<Text>();
+        _textComponent.text = "";
+        Foi = false;
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (Foi == true)
+        {
+            if (!_isDialoguePlaying)
+            {
+                _isDialoguePlaying = true;
+                StartCoroutine(StartDialogue());
+            }
+        }else{
+		}
+		
+    }
+    private IEnumerator StartDialogue()
+    {
+        int dialogueLength = DialogueStrings.Length;
+        int currentDialogueIndex = 0;
+
+        while (currentDialogueIndex < dialogueLength)
+        {
+            yield return StartCoroutine(DisplayString(DialogueStrings[currentDialogueIndex]));
+            currentDialogueIndex++;
+        }
+        _isDialoguePlaying = false;
+    }
+    private IEnumerator DisplayString(string stringToDisplay)
+    {
+        int stringLength = stringToDisplay.Length;
+        int currentCharacterIndex = 0;
+
+        _textComponent.text = "";
+
+        while (currentCharacterIndex < stringLength)
+        {
+            _textComponent.text += stringToDisplay[currentCharacterIndex];
+            currentCharacterIndex++;
+            if (currentCharacterIndex < stringLength)
+            {
+                if (Input.GetKey(DialogueInput))
+                {
+                    yield return new WaitForSeconds(SecondsBetweenCharacters * CharacterRateMultiplier);
+                }
+                else
+                {
+                    yield return new WaitForSeconds(SecondsBetweenCharacters);
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+        while (true)
+        {
+             if (Input.GetKeyDown(DialogueInput))
+            {
+                Texto.SetActive(false);
+            }    
+            yield return 0;
+        }
+        _textComponent.text = "";
+    }
+}
